1e82bc6a504c53e01f989cc3b2f495da2168f539
adamjking3 <adamjking3@gmail.com>
2019-07-02 15:41:07 -0700

    Refactor  with coherent variable names

--- update_data.py
+++ update_data.py
@@ -3,34 +3,36 @@ import ssl
 import pandas as pd
 import os
 
-final_fmt = '%Y-%m-%d %H:%M'
+final_date_format = '%Y-%m-%d %H:%M'
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url = "https://www.cryptodatadownload.com/cdd/Coinbase_BTCUSD_1h.csv"
-url2 = "https://www.cryptodatadownload.com/cdd/Coinbase_BTCUSD_d.csv"
+hourly_url = "https://www.cryptodatadownload.com/cdd/Coinbase_BTCUSD_1h.csv"
+daily_url = "https://www.cryptodatadownload.com/cdd/Coinbase_BTCUSD_d.csv"
 
 
-async def get_daily(url: str, fmt: str, fn: str):
-    c = pd.read_csv(url, header=1)
-    c = c.dropna(thresh=2)
-    c.columns = ['Date', 'Symbol', 'Open', 'High', 'Low', 'Close', 'VolumeFrom', 'VolumeTo']
-    c['Date'] = pd.to_datetime(c['Date'], format=fmt)
-    c['Date'] = c['Date'].dt.strftime(final_fmt)
+async def save_url_to_csv(url: str, date_format: str, file_name: str):
+    csv = pd.read_csv(url, header=1)
+    csv = csv.dropna(thresh=2)
+    csv.columns = ['Date', 'Symbol', 'Open', 'High', 'Low', 'Close', 'VolumeFrom', 'VolumeTo']
+    csv['Date'] = pd.to_datetime(csv['Date'], format=date_format)
+    csv['Date'] = csv['Date'].dt.strftime(final_date_format)
 
-    p = os.path.join('data', 'input', fn)
-    c.to_csv(p, index=False)
+    final_path = os.path.join('data', 'input', file_name)
+    csv.to_csv(final_path, index=False)
 
-    return c
+    return csv
 
 
-async def get_pages(url1: str, url2: str):
-    tasks = [get_daily(url1, '%Y-%m-%d %I-%p', 'coinbase-1h-btc-usd.csv'), get_daily(url2, '%Y-%m-%d', 'coinbase-1d-btc-usd.csv')]
-    done, pending = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)  # also FIRST_EXCEPTION and ALL_COMPLETED (default)
+async def save_as_csv(hourly_url: str, daily_url: str):
+    tasks = [save_url_to_csv(hourly_url, '%Y-%m-%d %I-%p', 'coinbase-1h-btc-usd.csv'),
+             save_url_to_csv(daily_url, '%Y-%m-%d', 'coinbase-1d-btc-usd.csv')]
+    # also FIRST_EXCEPTION and ALL_COMPLETED (default)
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
     print('>> done: ', done)
     print('>> pending: ', pending)  # will be empty if using default return_when setting
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_pages(url, url2))
+    loop.run_until_complete(save_as_csv(hourly_url, daily_url))
     loop.close()
