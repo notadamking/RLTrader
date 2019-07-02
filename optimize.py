febbb02529ac37160814e863a3d69eeddc7d54fc
ubuntu <rayanup3@gmail.com>
2019-07-02 12:17:47 +0530

    Enabled multiprocessing for Optuna.

--- optimize.py
+++ optimize.py
@@ -1,11 +1,39 @@
 import numpy as np
-
+import multiprocessing
 from lib.RLTrader import RLTrader
 
 np.warnings.filterwarnings('ignore')
 
-if __name__ == '__main__':
+
+def optimize_code():
     trader = RLTrader()
 
     trader.optimize()
+
+
+if __name__ == '__main__':
+    # The no of process you wanna launch
+    n_process = 2
+    # Database path param, recommended to replace with your own MySQL or PostgreSQL path
+    params = {
+        'params_db_path': 'sqlite:///data/params.db'
+    }
+
+    process = []
+    # multiprocessing
+    # creating processes
+    for i in range(n_process):
+        process.append(multiprocessing.Process(target=optimize_code(), kwargs=params))
+
+    # start processes
+    for p in process:
+        p.start()
+
+    # join
+    for p in process:
+        p.join()
+
+
+    trader = RLTrader(**params)
+
     trader.train(test_trained_model=True, render_trained_model=True)
