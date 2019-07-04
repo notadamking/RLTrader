f9fa7ecb521d9f0ea9608539a2119b0b092c1251
adamjking3 <adamjking3@gmail.com>
2019-07-04 01:11:12 -0700

    Remove unwanted comment

--- optimize.py
+++ optimize.py
@@ -7,5 +7,5 @@ np.warnings.filterwarnings('ignore')
 if __name__ == '__main__':
     trader = RLTrader()
 
-    # trader.optimize()
+    trader.optimize()
     trader.train(test_trained_model=True, render_trained_model=True)
