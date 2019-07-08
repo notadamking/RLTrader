660b29f119dc5a19469e658e160ad3b4d4c9a800
adamjking3 <adamjking3@gmail.com>
2019-07-07 19:13:07 -0700

    Add  to  and better handle lists

--- lib/data/features/transform.py
+++ lib/data/features/transform.py
@@ -14,30 +14,36 @@ def transform(iterable: Iterable, inplace: bool = True, columns: List[str] = Non
 
     if isinstance(transformed_iterable, pd.DataFrame):
         is_list = False
-        transformed_iterable.fillna(method='bfill', inplace=True)
     else:
         is_list = True
-        transformed_iterable = pd.DataFrame(transformed_iterable)
-        transformed_iterable.fillna(method='bfill', axis=1, inplace=True)
+        transformed_iterable = pd.DataFrame(transformed_iterable, columns=columns)
+
+    transformed_iterable.fillna(0, inplace=True)
 
     if transform_fn is None:
         raise NotImplementedError()
 
     if columns is None:
-        transformed_iterable = transform_fn(transformed_iterable)
-    else:
-        for column in columns:
-            transformed_iterable[column] = transform_fn(
-                transformed_iterable[column])
+        columns = transformed_iterable.columns
+
+    for column in columns:
+        transformed_iterable[column] = transform_fn(transformed_iterable[column])
+
+    transformed_iterable.fillna(method="bfill", inplace=True)
+    transformed_iterable[np.bitwise_not(np.isfinite(transformed_iterable))] = 0
 
     if is_list:
-        transformed_iterable = transformed_iterable.as_matrix()
+        transformed_iterable = transformed_iterable.values
 
     return transformed_iterable
 
 
 def max_min_normalize(iterable: Iterable, inplace: bool = True, columns: List[str] = None):
     return transform(iterable, inplace, columns, lambda t_iterable: (t_iterable - t_iterable.min()) / (t_iterable.max() - t_iterable.min()))
+
+
+def mean_normalize(iterable: Iterable, inplace: bool = True, columns: List[str] = None):
+    return transform(iterable, inplace, columns, lambda t_iterable: (t_iterable - t_iterable.mean()) / t_iterable.std())
 
 
 def difference(iterable: Iterable, inplace: bool = True, columns: List[str] = None):
